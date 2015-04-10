6b411a706c8eb49db152d3cba9e0a5370901b56a
SkateSoft <dylanredfield@verizon.net>
2015-04-10 13:06:34 -0400

    Changes

--- app/src/main/java/org/spfk12/bestgroup/mancala/Board.java
+++ app/src/main/java/org/spfk12/bestgroup/mancala/Board.java
@@ -1,11 +1,12 @@
 package org.spfk12.bestgroup.mancala;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.util.Log;
 import android.widget.GridView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Board {
     private int mBoard[][];
@@ -51,11 +52,6 @@ public class Board {
 
         mPlayerOneScoreText.setText("" + mPlayerOneScore);
         mPlayerTwoScoreText.setText("" + mPlayerTwoScore);
-/*        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
     }
 
     public void restartGame() {
@@ -71,8 +67,11 @@ public class Board {
         int startPlayer = player;
         boolean flag = false;
         boolean returnStatement = false;
-        boolean changeFlag = false;
-        boolean changeFlag2 = false;
+        int startPit1 = mPlayerOneScore;
+        int startPit2 = mPlayerTwoScore;
+        int startAfterPit1 = mBoard[0][5];
+        int startAfterPit2 = mBoard[1][0];
+        boolean goAgainCheck = false;
 
 
         if (player == 2)
@@ -83,70 +82,74 @@ public class Board {
         int counter = mBoard[player % 2][spot - 1];
 
 
-        mBoard[player % 2][spot - 1] = 0;
-        while (counter > 0) {
-            if (player == 1) {
-                if (spot < mBoard[1].length) {
-                    mBoard[1][spot] += 1;
-                    //checkRake(spot, player);
-                    counter--;
-                    spot++;
-                    checkRake(spot, startPlayer);
-/*                    if (checkRake(spot, startPlayer) == true) {
-                        //goAgain(spot, player);
-                        //returnStatement = true;
-                        Log.d("go again", "" + player);
-                    }*/
-                    updateDisplay();
-                } else if (spot == mBoard[1].length) {
-                    if (startPlayer == player) {
-                        mPlayerOneScore++;
+        if (mBoard[player % 2][spot - 1] != 0) {
+            mBoard[player % 2][spot - 1] = 0;
+            updateDisplay();
+            while (counter > 0) {
+                if (player == 1) {
+                    if (spot < mBoard[1].length) {
+                        mBoard[1][spot] += 1;
                         counter--;
-                        if (counter == 0) {
-                            mBoard[0][5] -= 1;
+                        spot++;
+                    } else if (spot == mBoard[1].length) {
+                        if (startPlayer == player) {
+                            mPlayerOneScore++;
+                            counter--;
+                            if (counter == 0) {
+                                mBoard[0][5] -= 1;
+                            }
                         }
+                        player = 2;        // Loop up to Player 2's row
                     }
-                    player = 2;        // Loop up to Player 2's row
-                    changeFlag = true;
                 }
-            }
 
-            if (player == 2) {
-                if (spot > 0) {
-                    // Makes sure it doesn't loop over the same spot twice
-                    if (mBoard[0][spot - 1] == 0 && flag == false) {
-                        spot--;
-                        flag = !flag;
-                    } else {
-                        mBoard[0][spot - 1] += 1;
-                        spot--;
-                        counter--;
+                if (player == 2) {
+                    if (spot > 0) {
+                        // Makes sure it doesn't loop over the same spot twice
+                        if (mBoard[0][spot - 1] == 0 && !flag && startPlayer == player) {
+                            spot--;
+                            flag = !flag;
+                        } else {
+                            mBoard[0][spot - 1] += 1;
+                            spot--;
+                            counter--;
+                        }
 
-                        updateDisplay();
+                    } else if (spot == 0) {
+                        if (startPlayer == player) {
+                            mPlayerTwoScore++;
+                            counter--;
+
+                        }
+                        //score2 = true;
+                        player = 1;        // Loop down to Player 1's row
                     }
-                } else if (spot == 0) {
-                    if (startPlayer == player) {
-                        mPlayerTwoScore++;
-                        counter--;
-
-                    }
-                    //score2 = true;
-                    player = 1;        // Loop down to Player 1's row
-                    changeFlag2 = true;
                 }
+
             }
-        }
-        if(counter == 0) {
-            if(changeFlag) {
-                spot--;
-            } if(changeFlag2) {
-                spot--;
+            goAgainCheck = checkRake(spot, player, startPlayer);
+            if (mPlayerOneScore > startPit1 && startAfterPit1 == mBoard[0][5] && !goAgainCheck) {
+                returnStatement = true;
+            } else if (mPlayerTwoScore > startPit2 && startAfterPit2 == mBoard[1][0] && !goAgainCheck) {
+                returnStatement = true;
             }
-            //checkRake();
-            Log.d("fuck", "Spot: " + spot + " Player: " + player);
+            updateDisplay();
+        } else {
+/*            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setMessage("Select a spot containing stones!")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //do things
+                        }
+                    });
+            builder.setTitle("Invalid Move!");
+            AlertDialog alert = builder.create();
+            alert.show();*/
+            returnStatement = true;
         }
-        updateDisplay();
-        //goAgain(spot, player);
+
+//        goAgain(spot, player);
         return returnStatement;
     }
 
@@ -170,37 +173,36 @@ public class Board {
         else if (spot == 6)
             return 1;
         else {
-            return -10;
+            return -20;
         }
     }
 
-    private boolean checkRake(int finalSpot, int player)//to be implemented in the move class
+    private boolean checkRake(int finalSpot, int player, int startPlayer)//to be implemented in the move class
     {
+        boolean returnValue = false;
 
-/*        int oppositeSpot = 0;
-        if (player == 1) {
+        if (player == 1 && player == startPlayer && mBoard[1][finalSpot - 1] == 1 && mBoard[0][finalSpot - 1] > 0) {
+            mPlayerOneScore += mBoard[0][finalSpot - 1];
+            mPlayerOneScore += mBoard[1][finalSpot - 1];
+            mBoard[1][finalSpot - 1] = 0;
+            mBoard[0][finalSpot - 1] = 0;
+            returnValue = true;
+        }
+        if (player == 2 && player == startPlayer && mBoard[0][finalSpot] == 1 && mBoard[1][finalSpot] > 0) {
+            mPlayerTwoScore += mBoard[1][finalSpot];
+            mPlayerTwoScore += mBoard[0][finalSpot];
+            mBoard[0][finalSpot] = 0;
+            mBoard[1][finalSpot] = 0;
 
-            Log.d("rake", "spot: " + finalSpot + " Player: " + player);
-            /*oppositeSpot = rakeConvert(player, finalSpot);
-            if (mBoard[0][finalSpot] == 1 && mBoard[1][oppositeSpot] > 0) {
-                mBoard[0][finalSpot] += mBoard[1][oppositeSpot];
-                mBoard[1][oppositeSpot] = 0;
-                return true;
-            }
+            returnValue = true;
+
+            //System.out.print("Falala");
+
         }
 
-        if (player == 2) {
+        checkWinner();
+        return returnValue;
 
-            Log.d("rake", "spot: " + finalSpot + " Player: " + player);
-             oppositeSpot = rakeConvert(player, finalSpot);
-            if (mBoard[1][finalSpot] == 1 && mBoard[0][oppositeSpot] > 0) {
-                mBoard[1][finalSpot] += mBoard[0][oppositeSpot];
-                mBoard[0][oppositeSpot] = 0;
-                return true;
-            }
-        }*/
-
-        return false;
     }
 
     private int rakeConvert(int player, int spot) {
@@ -242,6 +244,71 @@ public class Board {
             }
         }
         return -1;
+    }
+
+    public void checkWinner() {
+        boolean repeatFlag = false;
+        boolean repeatFlag2 = false;
+        if (mPlayerOneScore >= 25) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setMessage("Player One Has Won!")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //do things
+                        }
+                    });
+            builder.setTitle("Winner!");
+            AlertDialog alert = builder.create();
+            alert.show();
+        } else if (mPlayerTwoScore >= 25) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setMessage("Player One Has Won!")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //do things
+                        }
+                    });
+            builder.setTitle("Winner!");
+            AlertDialog alert = builder.create();
+            alert.show();
+        } else {
+            for (int i = 0; i < mBoard[0].length; i++) {
+                if (mBoard[0][i] > 0) {
+                    repeatFlag = true;
+                    break;
+                }
+            }
+            for (int j = 0; j < mBoard[0].length; j++) {
+                if (mBoard[1][j] > 0) {
+                    repeatFlag2 = true;
+                }
+            }
+
+            if (!repeatFlag || !repeatFlag2) {
+                String winner;
+                if (mPlayerOneScore > mPlayerTwoScore) {
+                    winner = "One";
+                } else if (mPlayerTwoScore > mPlayerOneScore) {
+                    winner = "Two";
+                } else {
+                    winner = "tie";
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage("Player " + winner + " Has Won!")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //do things
+                            }
+                        });
+                builder.setTitle("Winner!");
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+
+        }
     }
 
     private boolean goAgain(int finalCol, int player) {
